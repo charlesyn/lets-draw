29d5b734b4d52be2c3bc7573467e549babd389b1
charlesyn <charlesyniu@gmail.com>
2018-07-26 23:41:38 -0400

    Place Canvas as component of Index page, Create a Room Join Vue component

---
+++ src/components/Index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <server-join/>
+    <canvas-vue/>
+  </div>
+</template>
+
+<script>
+import CanvasVue from './Canvas'
+import ServerJoin from './ServerJoin'
+
+export default {
+  components: {
+    CanvasVue,
+    ServerJoin
+  },
+  name: 'Index',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/ServerJoin.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-container>
+    <v-layout>
+      <v-flex md4 offset-md4>
+        <v-text-field
+          label='Room'
+          placeholder='Join a Room'
+          v-model='room'
+          @keyup.enter='joinServer'
+          >
+        </v-text-field>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+import io from 'socket.io-client'
+export default {
+  name: 'ServerJoin',
+  mounted () {
+    this.socket = io('localhost:8081')
+  },
+  data () {
+    return {
+      room: '',
+      socket: null
+    }
+  },
+  methods: {
+    joinServer () {
+      this.socket.emit('room', this.room)
+    }
+  }
+}
+
+</script>
+
+<style scoped>
+
+</style>
